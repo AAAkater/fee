706a217367dbd935326e1a5e8102aee7d635f9d7
qll-NO.1 <Zyj647@126.com>
2025-03-27 12:14:26 +0800

    Update create.py
    改正(应该？)了表的引用路径

--- app/service/create.py
+++ app/service/create.py
@@ -4,20 +4,20 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import Engine, select
 from sqlmodel import Session
 
-from example.app.models.users import UserTable
+from db.tables import User
 
 def create_user(register_form:OAuth2PasswordRequestForm = Depends()):
     # 创建数据库会话
     with Session(Engine) as session:
         # 检查用户是否存在
-        user = session.exec(select(UserTable).where(UserTable.username == register_form.username)).first()
+        user = session.exec(select(User).where(User.username == register_form.username)).first()
         if user:
             raise HTTPException(status_code=400, detail="用户名已存在")
         
         # 创建新用户
-    new_user = UserTable(
+    new_user = User(
         username=register_form.username,
-        password=register_form.password 
+        password_hash=register_form.password_hash
     )
     session.add(new_user)
     session.commit()

062854a8daaf91f9332a2a9a586cdfcc4797d511
qll-NO.1 <Zyj647@126.com>
2025-03-27 12:03:52 +0800

    Update select.py
    上传根据用户名查找用户函数

--- app/service/select.py
+++ app/service/select.py
@@ -1,1 +1,16 @@
 
+from sqlalchemy import Engine, select
+from sqlmodel import Session
+from example.app.models.users import UserTable
+
+
+def select_user_from_username(username:str,password:str):
+    with Session(Engine) as session:
+        
+        # 从数据库中查询是否有该用户
+        user = session.exec(select(UserTable).where(UserTable.username == username)).first()
+        
+        if user and user.password == password:
+            return True
+    
+    return False

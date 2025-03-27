72ff9060a7467acb61b823e331ba7915d2d1036e
qll-NO.1 <Zyj647@126.com>
2025-03-27 12:16:32 +0800

    Update select.py
    改正了表的引用路径

--- app/service/select.py
+++ app/service/select.py
@@ -1,16 +1,16 @@
 
 from sqlalchemy import Engine, select
 from sqlmodel import Session
-from example.app.models.users import UserTable
+from db.tables import User
 
 
-def select_user_from_username(username:str,password:str):
+def select_user_from_username(username:str,password_hash:str):
     with Session(Engine) as session:
         
         # 从数据库中查询是否有该用户
-        user = session.exec(select(UserTable).where(UserTable.username == username)).first()
+        user = session.exec(select(User).where(User.username == username)).first()
         
-        if user and user.password == password:
+        if user and user.password_hash == password_hash:
             return True
     
     return False

c1f06c7482f0c7e7431cae5a215e5e8a6817e73d
qll-NO.1 <Zyj647@126.com>
2025-03-27 11:52:34 +0800

    Update users.py
    好像不会用我这个qaq

--- app/models/users.py
+++ app/models/users.py
@@ -1,9 +1,1 @@
-# 用户表
-from pydantic import Field
-from sqlmodel import SQLModel
 
-
-class UserTable(SQLModel,table=True):
-    id:int = Field(default=None,primary_key=True)
-    username:str
-    password:str

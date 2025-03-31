a5fbd16fc34b6ecc730b62526c0a6fcd4c2e7306
AAAkater <1318382761@qq.com>
2025-03-31 17:05:16 +0800

    ✨ feat: new create user service

---
+++ app/service/user.py
@@ -0,0 +1,10 @@
+from app.db.postgres_client import SessionDep
+from app.db.tables import User
+from app.models.db.user import UserCreate
+
+
+def create_new_user(*, sessions: SessionDep, new_user_info: UserCreate):
+    new_user = User.model_validate(new_user_info)
+
+    sessions.add(new_user)
+    sessions.commit()

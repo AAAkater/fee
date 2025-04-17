19fb50cf922cdae2703f1766d5e6f65e287c3850
AAAkater <1318382761@qq.com>
2025-04-17 22:05:44 +0800

    🦄 refactor(login): add logger and add to routing group "captcha"

--- app/api/routes/v1/__init__.py
+++ app/api/routes/v1/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes.v1 import chat, user
+from app.api.routes.v1 import captcha, chat, user
 
 v1_router = APIRouter(prefix="/v1")
 
 v1_router.include_router(user.router)
 v1_router.include_router(chat.router)
+v1_router.include_router(captcha.router)
--- app/api/routes/v1/captcha.py
+++ app/api/routes/v1/captcha.py
@@ -1,33 +1,39 @@
+from typing import Literal
+
 from fastapi import APIRouter, HTTPException
+
+from app.models.response import ResponseBase
+from app.models.response.user import CaptchaItem
+from app.utils.logger import logger
 from app.utils.security import Captcha, CaptchaInfo
 
-router = APIRouter()
+router = APIRouter(tags=["captcha"])
 
 
-@router.get("/captcha", response_model=CaptchaInfo)
-async def generate_image_captcha(code_len: int = 4):
+@router.get(
+    "/captcha/{captcha_type}",
+    response_model=ResponseBase[CaptchaItem],
+    summary="获取图形验证码",
+)
+async def generate_image_captcha(captcha_type: Literal["img", "email"] = "img"):
     """生成图形验证码接口"""
+
+    if captcha_type != "img":
+        logger.error(f"验证码类型不支持: {captcha_type}")
+        raise HTTPException(status_code=400, detail="验证码类型不支持")
     try:
         # 创建图形验证码对象(type为img)
-        captcha = Captcha(code_len=code_len, type="img")
+        captcha = Captcha(type="img")
 
         # 获取验证码信息(包括id、code、base64)
-        captcha_info = captcha.get_captcha()
-
-        # 确保返回的 base64 存在以及包含 MIME 前缀(浏览器才能识别这是一个图片)
-        if captcha_info.base64 and not captcha_info.base64.startswith(
-            "data:image/png"
-        ):
-            captcha_info.base64 = f"data:image/png;base64,{captcha_info.base64}"
-
+        captcha_info: CaptchaInfo = captcha.get_captcha()
+        logger.info(f"生成验证码成功: {captcha_info.code=}")
+        return ResponseBase[CaptchaItem](
+            data=CaptchaItem(
+                captcha_id=captcha_info.id,
+                captcha_img_base64=captcha_info.base64 or "",
+            ),
+        )
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"验证码生成失败: {str(e)}")
-
-    return {
-        "code": "0",
-        "msg": "ok",
-        "data": {
-            "captcha_id": captcha_info.id,
-            "captcha_img_base64": captcha_info.base64,
-        },
-    }
+        logger.error(f"生成验证码失败:\n {e}")
+        raise HTTPException(status_code=500, detail="验证码生成失败")

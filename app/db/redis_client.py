dbc684f1a30e15a99263c766d1f3ca3bc300f3b2
AAAkater <1318382761@qq.com>
2025-04-17 22:07:48 +0800

    🦄 refactor: change redis connection configuration parameters

--- app/db/redis_client.py
+++ app/db/redis_client.py
@@ -1,11 +1,18 @@
 from redis import StrictRedis
 
 from app.core.config import settings
+from app.utils.logger import logger
 
-r = StrictRedis(host=str(settings.REDIS_URL))
+r = StrictRedis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB,
+    decode_responses=True,
+)
 
 
 if __name__ == "__main__":
-    print(r.ping())
-    r.set("test", "test")
-    print(r.get("test"))
+    print(f"{settings.REDIS_URL=}")
+
+    if r.ping():
+        logger.success("Redis连接成功")

9290a7fbb7f0d29ae382bc14603de840bc552ea0
AAAkater <1318382761@qq.com>
2025-04-17 22:06:47 +0800

    ✨ feat(login): add captcha response item class

--- app/models/response/user.py
+++ app/models/response/user.py
@@ -1,8 +1,11 @@
-import token
-
 from pydantic import BaseModel
 
 
 class TokenItem(BaseModel):
     access_token: str
     token_type: str = "Bearer"
+
+
+class CaptchaItem(BaseModel):
+    captcha_id: str
+    captcha_img_base64: str

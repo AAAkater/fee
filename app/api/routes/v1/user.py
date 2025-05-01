3476301343cc08ff0001fd47e5d0a673dd5e3e52
AAAkater <1318382761@qq.com>
2025-05-01 21:03:58 +0800

    🦄 refactor(login): mv to services folder

--- app/api/routes/v1/user.py
+++ app/api/routes/v1/user.py
@@ -8,7 +8,7 @@ from app.models.db_models.user import UserCreate
 from app.models.request.user import UserRegisterBody
 from app.models.response import ResponseBase
 from app.models.response.user import TokenItem
-from app.service.user import create_new_user, get_user_by_username
+from app.services import user_service
 from app.utils import security
 from app.utils.logger import logger
 from app.utils.security import Captcha
@@ -26,7 +26,7 @@ async def login(
     session: SessionDep,
     user_form: OAuth2PasswordRequestForm = Depends(),
 ) -> ResponseBase[TokenItem]:
-    user = get_user_by_username(
+    user = user_service.get_user_by_username(
         session=session,
         username=user_form.username,
     )
@@ -67,14 +67,16 @@ async def register(session: SessionDep, new_user: UserRegisterBody):
         )
 
     # 检查用户名是否已存在
-    user = get_user_by_username(session=session, username=new_user.username)
+    user = user_service.get_user_by_username(
+        session=session, username=new_user.username
+    )
     if user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="用户名已存在"
         )
 
     try:
-        create_new_user(
+        user_service.create_new_user(
             session=session,
             new_user_info=UserCreate(
                 username=new_user.username,
